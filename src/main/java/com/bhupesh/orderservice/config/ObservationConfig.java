a20a77d108c2216fe23dfd63b9304d9aa50d567e
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2025-02-01 00:02:11 +0530

    signing-off

---
+++ src/main/java/com/bhupesh/orderservice/config/ObservationConfig.java
@@ -0,0 +1,27 @@
+package com.bhupesh.orderservice.config;
+
+import com.bhupesh.orderservice.event.OrderPlacedEvent;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class ObservationConfig {
+
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
+    @PostConstruct
+    public void setObservationForKafkaTemplate() {
+        kafkaTemplate.setObservationEnabled(true);
+    }
+
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry registry) {
+        return new ObservedAspect(registry);
+    }
+}
